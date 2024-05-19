31241d674197a35ad368ac5f3f2d22c2268e95c1
akshayJais21 <166414761+akshayJais21@users.noreply.github.com>
2024-05-19 20:16:34 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,11 @@
+//https://www.lintcode.com/problem/746/descriptionclass
+//https://leetcode.com/problems/design-tic-tac-toe/description/
 class TicTacToe {
+
     int[][] mat ;
     int turn ;
     int winner;
-    
+
     // denote  0 for X and 1 for other
 
     /** Initialize your data structure here. */
@@ -11,58 +14,67 @@ class TicTacToe {
         this.turn = 0;
         this.winner = -1;
         for(int i = 0 ; i < 3 ; i++)
-        for(int j = 0 ; j < 3 ; j++)
-            mat[i][j] = -1;
+            for(int j = 0 ; j < 3 ; j++)
+                mat[i][j] = -1;
     }
-    
-    public boolean move(int row, int col) throws AlreadyTakenException, GameEndException {
-        if(winner != -1)
-            throw new GameEndException("game end");
-        if( allPlacesTaken())
-            {
+
+    public boolean moveV2(int row, int col) throws AlreadyTakenException, GameEndException {
+        try {
+            if (winner != -1)
+                throw new GameEndException("game end");
+            if (allPlacesTaken()) {
                 System.out.println("it's a draw");
                 return false;
             }
-        if(turn%2 == 0 ){
-            if(canMove(row,col))
-                process(row,col,'X');
-            else
-                throw new AlreadyTakenException("already");
-             winner = checkForWinner();
-             if( winner!= -1 ) 
-                return true;  
-             turn ++;
-        }else{
-            if(canMove(row,col))
-                process(row,col,'O');
-            else
-                throw new AlreadyTakenException("already");
+            if (turn % 2 == 0) {
+                if (canMove(row, col))
+                    process(row, col, 'X');
+                else
+                    throw new AlreadyTakenException("already");
+                winner = checkForWinner();
+                if (winner != -1) {
+                    printWinner();
+                    return true;
+                }
+                turn++;
+            } else {
+                if (canMove(row, col))
+                    process(row, col, 'O');
+                else
+                    throw new AlreadyTakenException("already");
                 // means pos already taken
-             winner = checkForWinner();
-             if( winner!= -1 ) 
-                return true;  
-             turn ++;
-        }    
+                winner = checkForWinner();
+                if (winner != -1) {
+                    printWinner();
+                    return true;
+                }
+                turn++;
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+            if(e instanceof GameEndException)
+                reset();
+        }
         return false;
     }
 
-    public boolean allPlacesTaken(){
+    private boolean allPlacesTaken(){
         for(int i = 0; i < 3 ; i++)
-        for(int j = 0 ; j < 3 ; j++)
-        {
-            if( mat[i][j] == -1 )
-                return false;
-        }
+            for(int j = 0 ; j < 3 ; j++)
+            {
+                if( mat[i][j] == -1 )
+                    return false;
+            }
         return true;
     }
-    public boolean canMove(int row,int col){
+    private boolean canMove(int row,int col){
         if(mat[row][col] == -1)
             return true;
         return false;
 
     }
 
-    public void process(int row,int col, char c){
+    private void process(int row,int col, char c){
         int no ;
         if( c == 'X'){
             no = 0 ;
@@ -72,10 +84,10 @@ class TicTacToe {
             no = 1;
             mat[row][col] = no;
         }
-        
+
     }
 
-    public int checkForWinner(){
+    private int checkForWinner(){
 
         if( mat[0][0] != -1 && mat[0][0] == mat[0][1] && mat[0][1] == mat[0][2])
             return mat[0][0];
@@ -83,7 +95,7 @@ class TicTacToe {
             return mat[1][0];
         if( mat[2][0] != -1 && mat[2][0] == mat[2][1] && mat[2][1] == mat[2][2])
             return mat[2][0];
-        
+
 
         if( mat[0][0] != -1 && mat[0][0] == mat[1][0]   && mat[1][0] == mat[2][0] )
             return mat[0][0];
@@ -91,7 +103,7 @@ class TicTacToe {
             return mat[0][1];
         if( mat[0][2] != -1 && mat[0][2] == mat[1][2]   && mat[1][2] == mat[2][2] )
             return mat[0][2];
-        
+
         if(mat[0][0] != -1 &&  mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2])
             return mat[0][0];
         if( mat[0][2] != -1 && mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0])
@@ -99,15 +111,36 @@ class TicTacToe {
         return -1;
 
     }
-    
-    public void print(){
+
+    private void print(){
+        System.out.println("matrix curr state");
         for(int i = 0 ; i < 3 ; i++)
-        { 
+        {
             for(int j = 0 ; j < 3 ; j++){
-            System.out.print(mat[i][j] + " ");
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println("\n");
         }
-        System.out.println("\n");
-        }
+    }
+    private void reset(){
+        this.turn = 0;
+        this.winner = -1;
+        for(int i = 0 ; i < 3 ; i++)
+            for(int j = 0 ; j < 3 ; j++)
+                mat[i][j] = -1;
+
+    }
+    public void move(int row, int col) throws GameEndException, AlreadyTakenException {
+        moveV2(row,col);
+        print();
+
+    }
+    public void printWinner(){
+        if(this.winner == 0)
+            System.out.println("X has won");
+        else if( this.winner == 1)
+            System.out.println("O has won");
+
     }
 
 
@@ -126,22 +159,25 @@ class GameEndException extends Exception {
 
 
 class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws GameEndException, AlreadyTakenException {
         TicTacToe t = new TicTacToe();
-        t.print();
-    
-// t.move(0, 0); // X turn
-// t.move(1, 0); // O trun 
-// t.move(1, 1); // X turn
-// t.move(2, 0); // O turn
-// t.move(2, 2); // X turn and win
-// t.move(0, 0);  //throw GameEndException
-// t.move(0, 0); // X turn
-// t.move(0, 0); // throw AlreadyTakenException
-// t.move(1, 0); // O turn
-// t.move(1, 1); // X turn
-// t.move(2, 0); // o turn
-// t.move(2, 2); // X turn and win
+
+ t.move(0, 0); // X turn
+
+ t.move(1, 0); // O trun
+ t.move(1, 1); // X turn
+ t.move(2, 0); // O turn
+ t.move(2, 2); // X turn and win
+        // put this exception
+        t.move(0, 0);
+
+ t.move(0, 0); // X turn
+
+ t.move(0, 0); // throw AlreadyTakenException
+ t.move(1, 0); // O turn
+ t.move(1, 1); // X turn
+ t.move(2, 0); // o turn
+ t.move(2, 2); // X turn and win
 // Output:
 // x player wins!
 // x player wins!
